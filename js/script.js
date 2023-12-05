3bbd234587de661677ac5435d67d6bdea05be2ef
mixzoy <abuldigirov@inbox.ru>
2023-12-05 18:41:48 +0300

    Перенос проекта на github

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+document.getElementById("instagram-links-btn").addEventListener("click", function(){
+    document.getElementById("instagram-links").classList.toggle("open")
+    document.getElementById("telegram-links").classList.remove("open")
+})
+// document.getElementById("youtube-links-btn").addEventListener("click", function(){
+    
+// //     document.getElementById("youtube-links").classList.toggle("open")
+// //     document.getElementById("instagram-links").classList.remove("open")
+// //     document.getElementById("telegram-links").classList.remove("open")
+// // })
+
+document.getElementById("telegram-links-btn").addEventListener("click", function(){
+    document.getElementById("telegram-links").classList.toggle("open")
+    document.getElementById("instagram-links").classList.remove("open")
+})
+
